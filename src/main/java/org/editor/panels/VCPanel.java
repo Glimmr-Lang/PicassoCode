2e4319d2ac35ede7ba012bb576c73e4fc371d54a
hexaredecimal <mfanakagama@gmail.com>
2025-05-20 18:07:31 +0200

    panels: Added file, VC and plugin panels

---
+++ src/main/java/org/editor/panels/VCPanel.java
@@ -0,0 +1,17 @@
+package org.editor.panels;
+
+import java.awt.BorderLayout;
+
+/**
+ *
+ * @author hexaredecimal
+ */
+public class VCPanel extends DockablePanel {
+
+
+	public VCPanel() {
+		super(new BorderLayout(), "Git", "Git", "Use version control", "compare-git");
+		// TODO: Implement the code
+	}
+
+}
