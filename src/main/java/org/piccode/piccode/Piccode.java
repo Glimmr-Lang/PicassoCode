54613daa4854c55cd39637d63a40ae55d931f384
hexaredecimal <mfanakagama@gmail.com>
2025-07-31 14:02:41 +0200

    piccode: Setup the compiler

--- src/main/java/org/piccode/piccode/Piccode.java
+++ src/main/java/org/piccode/piccode/Piccode.java
@@ -1,8 +1,9 @@
 package org.piccode.piccode;
 
+import org.editor.AccessFrame;
 import org.editor.EditorWindow;
 import org.piccode.backend.Compiler;
-
+import org.piccode.piccodescript.ErrorAsciiKind;
 
 /**
  *
@@ -10,8 +11,11 @@ import org.piccode.backend.Compiler;
  */
 public class Piccode {
 
-    public static void main(String[] args) {
-			// Compiler.compile();
-			EditorWindow.the();
-		}
+	public static void main(String[] args) {
+		Compiler.exitOnError = false;
+		Compiler.errorKind = ErrorAsciiKind.EMACS_COMP_STYLE;
+		Compiler.out = AccessFrame.AccessFrameOutputStream.out;
+
+		EditorWindow.the();
+	}
 }
